c3b2c50f6ce570244925581ec889e901a7e3e4fa
akaddour <kaddouriaminne@gmail.com>
2025-10-04 00:50:31 +0100

    feat(products): implement pagination with page validation

--- controllers/productController.js
+++ controllers/productController.js
@@ -28,6 +28,18 @@ const getAllProducts = async (req, res) => {
       query = query.select("-__v");
     }
 
+    // Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numProducts = await Product.countDocuments();
+      if (skip >= numProducts) throw new Error("This page does not exist");
+    }
+
     const products = await query;
 
     res.status(200).json({
